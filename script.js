5f4038de3cab33412c8077bfcb1f42799155523d
lucy2n <lyusianaumenko@yandex.by>
2023-01-08 18:15:13 +0300

    Finish basic implementation of add and edit forms

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
+const content = document.querySelector('.content');
+
 const editPopup = document.querySelector('#popup-edit');
 const addPopup = document.querySelector('#popup-add');
-const closeButton = editPopup.querySelector('.popup__close-button');
-const editButton = document.querySelector('.profile__edit-button');
-const elements = document.querySelector('.elements');
-const addButton = document.querySelector('.profile__add-button');
+const closeButtonEdit = editPopup.querySelector('.popup__close-button');
+const closeButtonAdd = addPopup.querySelector('.popup__close-button');
+
+const profile = content.querySelector('.profile');
+const editButton = profile.querySelector('.profile__edit-button');
+const addButton = profile.querySelector('.profile__add-button');
 
 
 function closeEditPopup() {
     editPopup.classList.remove('popup_opened');
   }
 
+  function closeAddPopup() {
+    addPopup.classList.remove('popup_opened');
+  }
+
 editButton.addEventListener('click', function () {
     editPopup.classList.add('popup_opened');
-    closeButton.addEventListener('click', closeEditPopup); 
+    closeButtonEdit.addEventListener('click', closeEditPopup); 
   }); 
 
 
-const formElement = editPopup.querySelector('.popup__container');
+addButton.addEventListener('click', function () {
+    addPopup.classList.add('popup_opened');
+    closeButtonAdd.addEventListener('click', closeAddPopup);
+  });
 
-function formSubmitHandler (evt) {
+
+function formSubmitEdit (evt) {
     evt.preventDefault(); 
     
     const nameInput = formElement.querySelector('#name-info');
@@ -30,42 +42,53 @@ function formSubmitHandler (evt) {
     profileName.innerHTML = nameInput.value;
     profileSubline.innerHTML = jobInput.value;
 
-    console.log(profileName.value);
-    console.log(nameInput.value);
-    console.log(profileSubline.value);
-    console.log(jobInput.value);
-
     closeEditPopup();
 
 }
+ 
+const formElement = editPopup.querySelector('.popup__container');
+formElement.addEventListener('submit', formSubmitEdit);
 
-formElement.addEventListener('submit', formSubmitHandler);
-
+// https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg
 
 function addCards(nameValue, linkValue) {
-    const cards = document.createElement('div');
-    cards.classList.add('.element');
-    const linkCard = document.createElement('img');
-    linkCard.classList.add('element__image');
-    linkCard.textContent = linkValue;
-    const nameCard = document.createElement('h3');
-    nameCard.classList.add('element__title');
-    nameCard.textContent = nameValue;
-    const likeButton = document.createElement('button');
-    likeButton.classList.add('element__like-button');
-    cards.append(linkCard, nameCard, likeButton);
-    elements.append(cards);
+    const elements = document.querySelector('.elements');
+
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+
+    // cardElement.querySelector('.element__trash-button').addEventListener('click', function (evt) {
+    //     console.log(evt);
+    //  }); ;
+    console.log(nameValue + ' ' + linkValue);
+    cardElement.querySelector('.element__image').src = linkValue;
+    cardElement.querySelector('.element__title').textContent = nameValue;
+    console.log(cardElement);
+    cardElement.querySelector('.element__like-button').addEventListener('click', function (evt) {
+        evt.target.classList.toggle('element__like-button_active');
+     }); 
+     elements.append(cardElement);
 }
 
-function closeAddPopup() {
-    addPopup.classList.remove('popup_opened');
-  }
+
+function formSubmitAdd (evt) {
+    evt.preventDefault(); 
+    
+    const cardName = addFormElement.querySelector('#name-card');
+    const cardLink = addFormElement.querySelector('#link-card');
+
+    addCards(cardName.value, cardLink.value);
+    closeAddPopup();
+
+    cardLink.value = '';
+    cardName.value = '';
+
+}
+
+const addFormElement = addPopup.querySelector('.popup__container');
+addFormElement.addEventListener('submit', formSubmitAdd);
 
 
-addButton.addEventListener('click', function () {
-    addPopup.classList.add('popup_opened');
-    closeButton.addEventListener('click', closeAddPopup); 
-  }); 
 
 
 
