06e464afea5f11542f39fb4c14b06014dc4ed2ce
lucy2n <lyusianaumenko@yandex.by>
2023-01-08 22:28:42 +0300

    Add delete card function

--- script.js
+++ script.js
@@ -9,26 +9,23 @@ const profile = content.querySelector('.profile');
 const editButton = profile.querySelector('.profile__edit-button');
 const addButton = profile.querySelector('.profile__add-button');
 
-
-function closeEditPopup() {
-    editPopup.classList.remove('popup_opened');
-  }
-
-  function closeAddPopup() {
-    addPopup.classList.remove('popup_opened');
-  }
+function closePopup (popup) {
+    popup.classList.remove('popup_opened');
+}
 
 editButton.addEventListener('click', function () {
     editPopup.classList.add('popup_opened');
-    closeButtonEdit.addEventListener('click', closeEditPopup); 
-  }); 
-
+    closeButtonEdit.addEventListener('click', function () {
+        closePopup(editPopup);
+    }); 
+}); 
 
 addButton.addEventListener('click', function () {
     addPopup.classList.add('popup_opened');
-    closeButtonAdd.addEventListener('click', closeAddPopup);
-  });
-
+    closeButtonAdd.addEventListener('click', function () {
+        closePopup(addPopup);
+    });
+});
 
 function formSubmitEdit (evt) {
     evt.preventDefault(); 
@@ -39,11 +36,10 @@ function formSubmitEdit (evt) {
     const profileName = document.querySelector('.profile__name');
     const profileSubline = document.querySelector('.profile__author-subline');
 
-    profileName.innerHTML = nameInput.value;
-    profileSubline.innerHTML = jobInput.value;
+    profileName.textContent = nameInput.value;
+    profileSubline.textContent = jobInput.value;
 
-    closeEditPopup();
-
+    closePopup(editPopup);
 }
  
 const formElement = editPopup.querySelector('.popup__container');
@@ -51,25 +47,26 @@ formElement.addEventListener('submit', formSubmitEdit);
 
 // https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg
 
-function addCards(nameValue, linkValue) {
+function addCard (nameValue, linkValue) {
     const elements = document.querySelector('.elements');
 
     const cardTemplate = document.querySelector('#card-template').content;
     const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
 
-    // cardElement.querySelector('.element__trash-button').addEventListener('click', function (evt) {
-    //     console.log(evt);
-    //  }); ;
-    console.log(nameValue + ' ' + linkValue);
+    cardElement.querySelector('.element__trash-button').addEventListener('click', function (evt) {
+        cardElement.remove();
+     }); ;
     cardElement.querySelector('.element__image').src = linkValue;
     cardElement.querySelector('.element__title').textContent = nameValue;
-    console.log(cardElement);
     cardElement.querySelector('.element__like-button').addEventListener('click', function (evt) {
         evt.target.classList.toggle('element__like-button_active');
      }); 
      elements.append(cardElement);
 }
 
+function deleteCard () {
+
+}
 
 function formSubmitAdd (evt) {
     evt.preventDefault(); 
@@ -77,12 +74,11 @@ function formSubmitAdd (evt) {
     const cardName = addFormElement.querySelector('#name-card');
     const cardLink = addFormElement.querySelector('#link-card');
 
-    addCards(cardName.value, cardLink.value);
-    closeAddPopup();
+    addCard(cardName.value, cardLink.value);
+    closePopup(addPopup);
 
     cardLink.value = '';
     cardName.value = '';
-
 }
 
 const addFormElement = addPopup.querySelector('.popup__container');
